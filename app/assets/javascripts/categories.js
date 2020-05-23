3f7296ce350e27e15fdd8b59d7d01996a9eff374
fujinotakaaki <taka0207879576@gmail.com>
2020-05-24 00:19:10 +0900

    test deploy 3rd

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,6 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-const scrollBottom = () => {
+function scrollBottom() {
   let element = document.documentElement;
   let bottom = element.scrollHeight - element.clientHeight;
   $('html, body').animate({scrollTop: bottom}, 'fast' );
